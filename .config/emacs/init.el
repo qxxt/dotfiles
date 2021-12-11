21a0a30434c4c359299145a2a881279601e0fc11
qxxt <57898942+qxxt@users.noreply.github.com>
2021-12-11 15:09:45 +0700

    update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -16,17 +16,3 @@
 (defun open-url-android (url &rest ignore)
       (interactive "sURL: ")
       (shell-command (concat "am start -a android.intent.action.VIEW -d '" url "'")))
-
-(setq browse-url-browser-function 'open-url-android)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
